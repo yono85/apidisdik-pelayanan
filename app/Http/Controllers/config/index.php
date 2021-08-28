e85d60a5cf7145f8a31744539bd8fe98d29545e1
yono85 <yonocahyono85@gmail.com>
2021-08-28 20:40:05 +0700

    update first

---
+++ app/Http/Controllers/config/index.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Http\Controllers\config;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class index extends Controller
+{
+
+    
+    //create new id to insert data
+    public function createnewid($request)
+    {
+        $test = (int)$request['value'];
+        $length = ( (int)$request['length'] - 1);
+        $sprint = sprintf('%0'.$length.'s', 0);
+
+        $condition = [ 
+            10 . $sprint =>  9,
+            9 . $sprint  =>  8,
+            8 . $sprint  =>  7,
+            7 . $sprint  =>  6,
+            6 . $sprint  =>  5,
+            5 . $sprint  =>  4,
+            4 . $sprint  =>  3,
+            3 . $sprint  =>  2,
+            2 . $sprint  =>  1
+        ];
+
+        $sprintnew = strlen($test) === (int)$request['length'] ? substr($test, 1) : $test;
+
+        foreach($condition as $row => $val)
+        {
+            if( $test < $row )
+            {
+                $value = $val . sprintf('%0'.$length.'s', $sprintnew);;
+            }
+        }
+
+
+        return $value;
+    }
+
+
+    // default date for table
+    public function date()
+    {
+        return date('Y-m-d H:i:s', time());
+    }
+
+
+    //number
+    public function number($request)
+    {
+        return preg_replace('/\D/', '', $request);
+    }
+
+
+    //create new uniq (number and char A-Z)
+    public function createuniq($q)
+    {
+        $length = (int)$q['length'];
+        $value = (int)$q['value'];
+
+        //
+        $char = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' . $value;
+        $charlength = strlen($char);
+        $rand = '';
+
+        //
+        for ($i = 0; $i < $length; $i++)
+        {
+            $rand .= $char[rand(0, $charlength - 1)];
+        }
+        return $rand;
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+// $router->get('/', function () use ($router) {
+//     return $router->app->version();
+// });
+
+// $router->group(['middleware'=>'cekingrequest'],function($router)
+// {
+
+//     $router->get('/', function()
+//     {
+//         return 'ok';
+//     });
+
+// });
+
+
+//
+$router->group(['prefix' => 'api',  'middleware' => 'cekrequest'],function($router){
+    $router->post('/login', 'access\manage@login');
+});
+
+
+//
+$router->group(['prefix'=>'api', 'middleware'=>['cekrequest','auth']],function($router)
+{
+    $router->get('/profile', 'access\manage@profile');
+    $router->post('/logout', 'access\manage@logout');
+});
+
+
+//testing
+$router->post('/testing/upload', 'testing\upload\index@image');
+
+
+//any get url
+$router->get('/{any}', 'error\page\index@get');
+$router->post('/{any}', 'error\page\index@post');
+
+// Route::get('/{any}', 'errors\page\index@main')->where('any', '.*');
+// Route::post('/{any}', 'errors\page\index@main')->where('any', '.*');
